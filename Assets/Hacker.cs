3aa015e3c48a13ef50c3a6042c9b5a5902961bf7
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-09 22:38:24 +0200

    3

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class Hacker : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void ShowMainMenu()
     {
         Terminal.WriteLine("Access Granted.");
         Terminal.WriteLine("Bypass Options Generated..");
         Terminal.WriteLine("1 - Library");
         Terminal.WriteLine("2 - Police Station");
+    }
+    
+    void Start()
+    {
+        ShowMainMenu();
     }
 
     // Update is called once per frame
