c2d1d6a52daae3102d25792e0a4d736cc9e78bed
vogel2 <48958459+vogel2@users.noreply.github.com>
2021-01-09 21:39:59 +0200

    1

---
+++ Assets/Hacker.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hacker : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Terminal.WriteLine("Access Granted.");
+        Terminal.WriteLine("Bypass Options Generated..");
+        Terminal.WriteLine("1 - Library");
+        Terminal.WriteLine("2 - Police Station");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
